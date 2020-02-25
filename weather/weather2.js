8a70ae7d0331ae7cfec87ac564766adfc9848ae5
林奈央 <hayashinao@hayashinaonoMacBook-Pro.local>
2020-02-25 15:44:37 +0900

    不要なファイル削除

--- weather/weather2.js
+++
@@ -1,7 +0,0 @@
-function myFunction() {
-  var url = 'https://api.openweathermap.org/data/2.5/forecast?q=London&APPID=4b5774e9f3d2a07b84f0f2f88e486224';
-  var response = UrlFetchApp.fetch(url).getContentText();
-  var json = JSON.parse(response);
-  
-  Logger.log(json.description.text)
-}
